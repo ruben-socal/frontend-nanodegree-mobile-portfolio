68f35f4198bee9ff9254688b62d825f6b802c7bd
Ruben <ruben2045@gmail.com>
2017-02-18 12:07:09 -0800

    update restore main.js to original in order to debug scroll background not working

--- views/js/main.js
+++ views/js/main.js
@@ -500,11 +500,10 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
-  var items = document.querySelectorAll('.mover');
-  var cachedScrollTop = document.body.scrollTop;
 
+  var items = document.querySelectorAll('.mover');
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((cachedScrollTop / 1250) + (i % 5));
+    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
